3b2f615bcffa473770f402234e1540c4e9a55acf
dvmarchetto <davi.pazello.soares@escola.pr.gov.br>
2024-06-27 17:15:38 -0300

    atualização de cor

--- pingpong.js
+++ pingpong.js
@@ -1,11 +1,11 @@
 function setup(){
     //aqui vou criar minha mesa
-    createcanvas (600, 400);
+    createcanvas (600,400);
 
 } 
 
 function draw(){
     //aqui vou por a cor de mesa, em rgb
-    background (220, 20, 60);
+    background (220,20,60);
 
 }
