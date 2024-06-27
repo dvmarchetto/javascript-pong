da1eed7098809863ec63f63b6992b1daff652a63
dvmarchetto <davi.pazello.soares@escola.pr.gov.br>
2024-06-27 17:04:57 -0300

    a

--- pingpong.js
+++ pingpong.js
@@ -1,10 +1,11 @@
 function setup(){
-    //aqui vou criar minha "mesa"
-    createCanvas(600,400);
-}
+    //aqui vou criar minha mesa
+    createcanvas (600, 400);
+
+} 
 
 function draw(){
-    //aqui vou por a cor da "mesa"
-    background(255,255,255);
+    //aqui vou por a cor de mesa, em rgb
+    background (220, 20, 60);
 
 }
