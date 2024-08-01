94322b6e3191a0459bae1691baf6deeb3a8d3955
dvmarchetto <davi.pazello.soares@escola.pr.gov.br>
2024-08-01 16:27:23 -0300

    .

--- pingpong.js
+++ pingpong.js
@@ -1,45 +1,72 @@
 let xBolinha = 300;
 let yBolinha = 200;
 let tamBolinha = 25;
+let raioBolinha = tamBolinha/2;
 
-//config velocidade bolinha
+
 let xvelocidadeBolinha = 6;
 let yvelocidadeBolinha = 6;
 
+let xraquete = 5;
+let yraquete = 150;
+let larguraraquete = 10;
+let alturaraquete = 90;
+
 function setup(){
-    //aqui vou criar minha "mesa"
+   
     createCanvas(600,400);
 }
 
 function draw(){
-    //função responsável pelo "desenho e animação da mesa"
-    //aqui vou por a cor da "mesa"
-    //A cor da mesa é em rgb
+   
     background(128,128,0); 
-    //chamando a função cria bolinha para criar a bolinha
     criaBolinha(xBolinha, yBolinha, tamBolinha);
-    //chamando a função mov bolinha
     moveBolinha();
-    //chamando a borda
-    Borda();
+    bolinhaBorda();
+    criaraquete(xraquete, yraquete, larguraraquete, alturaraquete);
+    movimentaraquete();
+    colideraquete();
 }
 
-//função bolinha
-function criaBolinha(xBolinha, yBolinha,tamBolinha){
-    circle (xBolinha,yBolinha,tamBolinha);
+// função bolinha
+function criaBolinha(xBolinha, yBolinha, tamBolinha){
+    fill("red"); 
+    circle (xBolinha, yBolinha, tamBolinha);
 }
 
-//função move bolinha
+// função move bolinha
 function moveBolinha(){
-    xBolinha = xvelocidadeBolinha + xBolinha;
-    yBolinha = yvelocidadeBolinha + yBolinha;
+    xBolinha += xvelocidadeBolinha;
+    yBolinha += yvelocidadeBolinha;
 }
 
-function Borda(){
+function bolinhaBorda(){ 
     if (xBolinha > width || xBolinha < 0){
         xvelocidadeBolinha *= -1;
     }
     if (yBolinha > height || yBolinha < 0){
         yvelocidadeBolinha *= -1;
+    }
+}
+
+function criaraquete(xraquete, yraquete, larguraraquete, alturaraquete) {
+    fill("blue"); 
+    rect(xraquete, yraquete, larguraraquete, alturaraquete);
+}
+
+function movimentaraquete() {
+    if(keyIsDown(UP_ARROW)) {
+        yraquete -= 10;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        yraquete += 10;
+    }
+}
+
+function colideraquete() {
+
+    if(xBolinha - raioBolinha < xraquete + larguraraquete && yBolinha - raioBolinha < yraquete + alturaraquete && yBolinha + raioBolinha > yraquete){
+
+        xvelocidadeBolinha *= -1;
     }
 }
